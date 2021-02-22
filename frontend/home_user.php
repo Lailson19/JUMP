8deb50ff86be7f7423777bd5b1ead91ede6b0598
Lailson19 <contatolailsonandesson@gmail.com>
2021-02-22 04:40:58 -0300

    Atualização cadastro de vídeo principal

--- frontend/home_user.php
+++ frontend/home_user.php
@@ -87,7 +87,7 @@ if (!isset($_SESSION['nome'])) {
                                 <div class="col-md-4 p-2">
                                     <a href="./home_video.php">
                                         <div class="card">
-                                            <img class="card-img-top" src="<?php echo $video['capa_conteudo'] ?>" alt="<?php echo $video['assunto_conteudo'] ?>">
+                                            <img class="card-img-top" src="../img/capa/<?php echo $video['capa_conteudo'] ?>" alt="<?php echo $video['assunto_conteudo'] ?>">
                                             <div class="card-body">
                                                 <h5 class="card-title"><?php echo $video['titulo_conteudo'] ?></h5>
                                                 <p class="card-text">
